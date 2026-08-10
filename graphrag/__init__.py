7c370a86381b824340cf8f2eefd4d8330802a7da
Brett Weston <87893812+Wezo1976@users.noreply.github.com>
2026-08-11 06:17:33 +1000

    Revert "Building a GraphRAG indexing system components"

--- graphrag/__init__.py
+++
@@ -1,6 +0,0 @@
-"""GraphRAG package for local graph indexing and retrieval."""
-
-from .pipeline_orchestrator import GraphRAGPipeline
-from .query_engine import QueryEngine
-
-__all__ = ["GraphRAGPipeline", "QueryEngine"]
